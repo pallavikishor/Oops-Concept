88b17fdcbe2a6bf4a6f2ea2b53e5383dd3e02118
pallavikishor <kishorpallavi2@gmail.com>
2025-09-03 01:24:53 +0000

    encapsulation

--- Encapsulation/Encap.java
+++ Encapsulation/Encap.java
@@ -3,5 +3,37 @@
 package Encapsulation;
 
 public class Encap {
-    
+    public static void main(String[] args){
+        Pen p1 = new Pen(); // we have created a pen onject called p1
+
+        p1.setColor("blue");
+        System.out.println(p1.getColor());
+
+        p1.setTip(5);
+        System.out.println(p1.getTip());
+
+        //p1.setColor("Yellow");
+        p1.setColor("yellow");
+        System.out.println(p1.getColor());
+
+    }
+}
+
+class Pen{
+    private String color;
+    private int tip;
+
+    String getColor(){
+        return this.color;
+    }
+    int getTip(){
+        return this.tip;
+    }
+    void setColor(String newColor){
+        color = newColor;
+    }
+
+    void setTip(int newTip){
+        tip = newTip;
+    }
 }
