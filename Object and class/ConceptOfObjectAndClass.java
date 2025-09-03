60dc7cad67e1b6dd355d218be15f4e6b503e9e27
pallavikishor <kishorpallavi2@gmail.com>
2025-09-03 01:00:41 +0000

    Getters and Setters

--- Object and class/ConceptOfObjectAndClass.java
+++ Object and class/ConceptOfObjectAndClass.java
@@ -3,21 +3,21 @@ public class ConceptOfObjectAndClass {
         Pen p1 = new Pen(); // we have created a pen onject called p1
 
         p1.setColor("blue");
-        System.out.println(p1.color);
+        System.out.println(p1.getColor());
 
         p1.setTip(5);
-        System.out.println(p1.tip);
+        System.out.println(p1.getTip());
 
         //p1.setColor("Yellow");
-        p1.color = "Yellow";
-        System.out.println(p1.color);
+        p1.setColor("yellow");
+        System.out.println(p1.getColor());
 
     }
 }
 
 
 
-class Pen{
+/*class Pen{
     String color;
     int tip;
 
@@ -28,7 +28,35 @@ class Pen{
     void setTip(int newTip){
         tip = newTip;
     }
+}*/
+
+
+// Get: 
+// Set:
+// this: 
+
+
+class Pen{
+    private String color;
+    private int tip;
+
+    String getColor(){
+        return this.color;
+    }
+    int getTip(){
+        return this.tip;
+    }
+    void setColor(String newColor){
+        color = newColor;
+    }
+
+    void setTip(int newTip){
+        tip = newTip;
+    }
 }
+
+
+
 
 class Student{
     String name;
