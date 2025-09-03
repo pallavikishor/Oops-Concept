0dafdd24b7ac6a8156ec5358aa2fad48ef8f8fd3
pallavikishor <kishorpallavi2@gmail.com>
2025-09-03 02:11:21 +0000

    Types of constructor

--- Constructors/Construct.java
+++ Constructors/Construct.java
@@ -4,11 +4,18 @@
 // Constructors are only called once. at object creation.
 // Memory allocation happens when Constructor is called.
 
+
+// Types of Constructor
+// 1. Non-parameterized: No arguments, sets default values.
+// 2. Parameterized: Takes arguments, sets custom values.
+// 3. Copy Consstructor: Create new object by copying another object.
 package Constructors;
 
 public class Construct {
     public static void main(String[] args) {
-        Student s1 = new Student("pallavi");
+        Student s1 = new Student();
+        Student s2 = new Student("pallavi");
+        Student s3 = new Student(123);
         System.out.println(s1.name);
     }
 }
@@ -16,9 +23,17 @@ public class Construct {
 class Student {
     String name;
     int roll;
-
-    Student(String name) {
-        this.name = name;
+    Student() {
         System.out.println("Constructor is called...");
     }
+
+    Student(String name){
+        this.name = name;
+    }
+
+    Student(int roll){
+        this.roll = roll;
+    }
 }
+
+
