aab78575a771f0194d9509e4b177f056eec11a8f
pallavikishor <kishorpallavi2@gmail.com>
2025-09-03 01:52:03 +0000

    constructor

--- Constructors/Construct.java
+++ Constructors/Construct.java
@@ -7,17 +7,18 @@
 package Constructors;
 
 public class Construct {
-    Student s1 = new Student("pallavi");
-    
-    System.out.println(s1.name);
+    public static void main(String[] args) {
+        Student s1 = new Student("pallavi");
+        System.out.println(s1.name);
+    }
 }
 
-class Student{
+class Student {
     String name;
     int roll;
 
-    Student(String name){
+    Student(String name) {
         this.name = name;
-        //System.out.println();
+        System.out.println("Constructor is called...");
     }
 }
