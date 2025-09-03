1e211633d4fe40c54b99c32576c0351713fb3276
pallavikishor <kishorpallavi2@gmail.com>
2025-09-03 01:23:14 +0000

    Encapsulation

---
+++ Encapsulation/Encap.java
@@ -0,0 +1,7 @@
+//Encapsulation: It is defined as the wrapping up of data and mettods under a single unit. It is also empliment data hiding.
+
+package Encapsulation;
+
+public class Encap {
+    
+}
